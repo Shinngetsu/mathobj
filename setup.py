ac5d67e9331fba434ab188c31312201d0b105c40
Sinngetsu <mr.waterfield2@gmail.com>
2025-06-08 11:27:51 +0900

    setup,py - README.md encoding change to UTF-8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="mathobj",
     version="0.1.0",
     description="This module provides the MathObj class, which encapsulates the implementation of operator overrides.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     url="https://github.com/Shinngetsu/mathobj.git",
     packages=find_packages(),
     license="MIT",
